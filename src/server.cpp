ada2b2d2e26aa42017a996eddbd2a7043f2fbf62
limccart7 <145800942+limccart7@users.noreply.github.com>
2024-11-13 20:05:08 -0800

    Create server.cpp

---
+++ src/server.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <string>
+#include <fcntl.h>
+#include <termios.h>
+#include <sstream>
+
+using namespace std;
+
+#define PORT 8080
